c42f723b8b33b12adce6eb94ebab84121ce88814
Diamond-Au <497350746@qq.com>
2021-01-12 20:29:08 +0800

    fix:bug

--- tem/diskInformation/index.js
+++ tem/diskInformation/index.js
@@ -1,9 +1,23 @@
 const { getComputerDiskstatsInfo } = require("../../utils/index.js");
 const { renderDiskinfo } = require("../../utils/render.js");
-window.onload = function () {
-  let diskinfoMain = document.getElementById("diskinfo-main")
-  let diskinfo = getComputerDiskstatsInfo();
+
+function setDiskInfo(diskinfoMain, diskinfo) {
+
   diskinfo = diskinfo.split("\n");
   let template = renderDiskinfo(diskinfo)
   diskinfoMain.innerHTML = template
+}
+
+var timer;
+window.onload = function () {
+  let diskinfoMain = document.getElementById("diskinfo-main")
+  let diskinfo = getComputerDiskstatsInfo();
+  setDiskInfo(diskinfoMain, diskinfo);
+  timer = setInterval(() => {
+    setDiskInfo(diskinfoMain, diskinfo);
+  }, 10 * 1000);
 };
+
+window.onunload = function () {
+  clearInterval(timer)
+}
--- tem/innermemory/index.js
+++ tem/innermemory/index.js
@@ -1,8 +1,19 @@
 const { getComputerMemotyInfo } = require("../../utils/index.js");
 const { renderInnerMemoryInfo } = require("../../utils/render.js");
+var timer = null;
 window.onload = function () {
   const memoryInfoMain = document.getElementById("memory-info-main");
   const MemotyInfo = getComputerMemotyInfo();
   let template = renderInnerMemoryInfo(MemotyInfo[0]);
   memoryInfoMain.innerHTML = template;
+
+  timer = setInterval(() => {
+    renderInnerMemoryInfo(MemotyInfo[0]);
+    memoryInfoMain.innerHTML = template;
+  }, 5 * 1000)
+
 };
+
+window.onunload = function () {
+  clearInterval(timer);
+}
